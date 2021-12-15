617c512f9774ab42101893ac53fe69e8eb08e671
azula9713 <nnviduranga@gmail.com>
2021-12-15 18:59:10 +0530

    Added Git

--- src/App/Store/Store.js
+++ src/App/Store/Store.js
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../Features/User/UserSlice";
 import moviesReducer from "../Features/Movie/MovieSlice";
-import requestAPIReducer from "../Features/RequestAPI/RequestAPISlice";
 
 export default configureStore({
   reducer: {
     user: userReducer,
     movies: moviesReducer,
-    requestAPI: requestAPIReducer,
   },
 });
