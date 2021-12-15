1bfebc505a350e57a94fb429659e195e3ff7a26d
azula9713 <nnviduranga@gmail.com>
2021-12-15 13:16:38 +0530

    Optimized user validation

--- src/App/Api/Auth.api.js
+++ src/App/Api/Auth.api.js
@@ -19,6 +19,16 @@ export const getSessions = async () => {
   return res;
 };
 
+export const validateUser = async () => {
+  const res = await api.get("/sessions/validate", {
+    headers: {
+      authorization: `Bearer ${localStorage.getItem("accesstoken")}`,
+      "x-refresh": localStorage.getItem("refreshtoken"),
+    },
+  });
+  return res;
+};
+
 export const logoutUser = async () => {
   const res = await api.patch(
     "/sessions/logout",
@@ -26,6 +36,7 @@ export const logoutUser = async () => {
     {
       headers: {
         authorization: `Bearer ${localStorage.getItem("accesstoken")}`,
+        "x-refresh": localStorage.getItem("refreshtoken"),
       },
     }
   );
