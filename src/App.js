ea24d4e5aedf94150a221a3b9570b408f1def8ee
azula9713 <nnviduranga@gmail.com>
2021-12-15 19:18:28 +0530

    Testing Local Variable

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import "./App.css";
 
 import Login from "./Components/Auth/Login/Login";
 import MovieDetails from "./Components/Details/MovieDetails";
-import ContentDetails from "./Components/Details/MovieDetails";
 import Home from "./Components/Home/Home";
 import CommonLoader from "./Components/Loaders/CommonLoader";
 import Welcome from "./Components/Welcome/Welcome";
--- src/App/Utils/Variables.js
+++ src/App/Utils/Variables.js
@@ -1,4 +1,4 @@
-// const apiServer = "http://localhost:1337";
-const apiServer = "https://api.tsukiyomi.live";
+const apiServer = "http://localhost:1337";
+// const apiServer = "https://api.tsukiyomi.live";
 
 export { apiServer };
