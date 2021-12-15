fbd35475f74d6d977d315e6534b826ace1a2221e
azula9713 <nnviduranga@gmail.com>
2021-12-15 11:31:46 +0530

    Added and configured react-query

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import { Provider } from "react-redux";
 
 import "./index.css";
 import App from "./App";
 import store from "./App/Store/Store";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchIntervalInBackground: false,
+    },
+  },
+});
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </Provider>,
   document.getElementById("root")
 );
